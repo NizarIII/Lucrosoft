576d3ed36c75d6f73785e00bcfa25f87ff658b63
Nizar <nizar.soccer10@gmail.com>
2016-03-18 14:39:54 -0400

    test 3

--- Lucro/src/lucroApp/Main.java
+++ Lucro/src/lucroApp/Main.java
@@ -4,8 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Is it working with github?");
-		System.out.println("Test 1");
-		System.out.println("Test 2");
+		System.out.println("Test 3");
 	}
 
 }
